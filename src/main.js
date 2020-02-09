c4fd69970044ca146882e94bf00c94577f54b8df
Damian Matyjaszek <tougepl@gmail.com>
2020-02-09 15:42:32 +0100

    Add VueBootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "@/assets/scss/style.scss";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
